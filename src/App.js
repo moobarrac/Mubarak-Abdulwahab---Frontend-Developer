1b1f15ebbd72d41395da1c6cb2d499bf49a64b21
moobarrac <wahabajibola01@gmail.com>
2023-12-12 22:03:50 +0100

    render important components in the app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Banner, DataGrid, SearchForm, Popup } from "./components";
+import SpaceImage from "./styling/images/space.jpg";
 
 function App() {
+  const [selectedCapsule, setSelectedCapsule] = useState(null);
+
+  const handleSelectCapsule = (capsule) => {
+    setSelectedCapsule(capsule);
+  };
+
+  const handleClosePopup = () => {
+    setSelectedCapsule(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="relative bg-black text-white overflow-hidden min-h-screen"
+      style={{
+        backgroundImage: `url(${SpaceImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Banner />
+      <SearchForm />
+      <DataGrid onSelectCapsule={handleSelectCapsule} />
+      <Popup
+        isOpen={!!selectedCapsule}
+        close={handleClosePopup}
+        capsule={selectedCapsule}
+      />
     </div>
   );
 }
