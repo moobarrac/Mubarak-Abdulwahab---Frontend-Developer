25c60e6d5c3a33882213b93663cbcd86c5d8e7e1
moobarrac <wahabajibola01@gmail.com>
2023-12-12 10:47:09 +0100

    create the required component

---
+++ src/components/DataGrid.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DataGrid = () => {
+  return <div>DataGrid</div>;
+};
+
+export default DataGrid;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import DataGrid from "./DataGrid";
+import Popup from "./Popup";
+import SearchForm from "./SearchForm";
+import Banner from "./Banner";
+
+export { Banner, DataGrid, Popup, SearchForm };
