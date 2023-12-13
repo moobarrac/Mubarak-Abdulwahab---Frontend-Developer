1d100d789c722eb2141153e01af1a7024dac39a1
moobarrac <wahabajibola01@gmail.com>
2023-12-14 00:56:47 +0100

    add functionality to filtering using the redux store

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,38 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FormInput } from "../utils/FormInput";
+import {
+  fetchSpaceXData,
+  updateFilter,
+  firstPage,
+} from "../features/spaceXSlice";
 
 const SearchForm = () => {
+  const dispatch = useDispatch();
+  const { filters } = useSelector((state) => state.spaceX);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(firstPage());
+    dispatch(fetchSpaceXData());
+  };
+
+  const handleFilterChange = (filterName) => (event) => {
+    dispatch(updateFilter({ filterName, value: event.target.value }));
+  };
+
   return (
     <div className="flex flex-col items-center my-8 mb-16 animate-opacity">
       <h2 className="text-xl md:text-2xl lg:text-3xl text-center text-white font-bold mb-4 uppercase">
         Filter Capsules
       </h2>
-      <form className="w-[90%] max-w-xl space-y-4">
+      <form className="w-[90%] max-w-xl space-y-4" onSubmit={handleSubmit}>
         <div className="flex flex-wrap items-center justify-center gap-3 md:space-x-3">
           {/* Status Select */}
           <select
             id="status"
+            value={filters.status}
+            onChange={handleFilterChange("status")}
             className="h-12 flex-grow p-2 border-2 border-transparent rounded focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-200 transition-all duration-300 text-black"
           >
             <option value="">All</option>
@@ -22,10 +43,20 @@ const SearchForm = () => {
           </select>
 
           {/* Original Launch Input */}
-          <FormInput type="date" placeholder="Original Launch" />
+          <FormInput
+            type="date"
+            placeholder="Original Launch"
+            value={filters.originalLaunch}
+            onChange={handleFilterChange("original_launch")}
+          />
 
           {/* Type Input */}
-          <FormInput type="text" placeholder="Type e.g Dragon 1.0" />
+          <FormInput
+            type="text"
+            placeholder="Type e.g Dragon 1.0"
+            value={filters.type}
+            onChange={handleFilterChange("type")}
+          />
         </div>
 
         {/* Search Button */}
