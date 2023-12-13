35c7358d1f7c2f5e6273fdfe8cc320a80fd637a8
moobarrac <wahabajibola01@gmail.com>
2023-12-13 01:55:28 +0100

    change cpasule date format

---
+++ src/utils/dateFormatter.js
@@ -0,0 +1,23 @@
+export const convertDateFormat = (dateStr) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const dateObj = new Date(dateStr);
+  const day = dateObj.getDate();
+  const month = months[dateObj.getMonth()];
+  const year = dateObj.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
