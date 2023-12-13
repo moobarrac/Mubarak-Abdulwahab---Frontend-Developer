5be0bce6a983d1fd7301aec4f37388584c10df30
moobarrac <wahabajibola01@gmail.com>
2023-12-13 22:43:14 +0100

    add extra reducer for capsule api call

--- src/features/spaceXSlice.js
+++ src/features/spaceXSlice.js
@@ -36,7 +36,47 @@ const spaceXSlice = createSlice({
       state.pagination.offset = 0;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSpaceXData.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchSpaceXData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.capsules = action.payload;
+      })
+      .addCase(fetchSpaceXData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
+
+export const fetchSpaceXData = createAsyncThunk(
+  "spaceX/fetchData",
+  async (_, { getState, rejectWithValue }) => {
+    const { filters, pagination } = getState().spaceX;
+    const query = new URLSearchParams(filters).toString();
+    const paginationQuery = `limit=${pagination.limit}&offset=${pagination.offset}`;
+
+    try {
+      const response = await fetch(
+        `http://localhost:8000/capsules?${query}&${paginationQuery}`,
+        {
+          headers: {
+            Authorization: `Bearer my_authorised_testing_token`,
+          },
+        }
+      );
+      if (!response.ok) throw new Error("Network response was not ok");
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 export const { updateFilter, previousPage, nextPage, firstPage } =
   spaceXSlice.actions;
